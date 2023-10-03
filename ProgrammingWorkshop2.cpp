977b7c547bfa86939d062c24918a4990ce45eef6
Max Wilkinson <mjswilkinson@outlook.com>
2023-10-03 13:14:01 +0100

    Start of project which saves and compares values in a text file, focusing on C++ basics such as if statements/switch case, vectors and so on.

---
+++ ProgrammingWorkshop2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+using namespace std;
+
+string word;
+int count;
+int numberOfWords;
+int numberOfSentences;
+string filename = "input.txt";
+vector<int> wordList; //adds word to vector if occured more than once?? keeps count??
+
+main()
+{
+
+}
+
+
+
+
+// In this homework, you will implement a program that
+// generates a summary of a given document (this could be based on the work from Task 1).
+// The summary will include the number of sentences, the number of words, and the
+// frequency of each word in the document. For this purpose, you are required to use built-in
+// types, vectors/arrays, and control structures to implement this task. The use of functions is
+// encouraged. This task will be assessed during the workshop session.. 
