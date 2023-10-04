3ec74366a85a3672201c4e2cb2c018d7cd70c4bb
Max Wilkinson <mjswilkinson@outlook.com>
2023-10-04 09:00:40 +0100

    text file

--- ProgrammingWorkshop2/ProgrammingWorkshop2.cpp
+++ ProgrammingWorkshop2/ProgrammingWorkshop2.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
 #include <string.h>
 #include <vector>
+#include <fstream>
 using namespace std;
 
+
+//task 2
 string word;
 int count;
 int numberOfWords;
 int numberOfSentences;
 string filename = "input.txt";
 vector<int> wordList; //adds word to vector if occured more than once?? keeps count??
+string fileContent;
 
-main()
+int Sentences(string fileData)
 {
+    cout << 0;
+}
+int NumberOfWords(string fileData)
+{
+    cout << 0;
+
+}
+int WordFrequency(string fileData)
+{
+    cout << 0;
 
 }
 
+int main()
+{
+    ifstream ReadFile(filename);
+    while (getline(ReadFile, fileContent))
+    {
+        cout << fileContent;
+    }
+    ReadFile.close();
 
-
+    Sentences(fileContent);
+    
+}
 
 // In this homework, you will implement a program that
 // generates a summary of a given document (this could be based on the work from Task 1).
